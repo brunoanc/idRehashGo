a3286bb37011146176ccac5ed88901e192477f0b
Bruno Ancona <brunoanconasala@gmail.com>
2023-02-12 17:55:50 -0600

    Bump version number

--- main.c
+++ main.c
@@ -199,7 +199,7 @@ bool generate_map(unsigned char *dec_data, const size_t size)
 
 int main(int argc, char **argv)
 {   
-    printf("idRehashLinux v1.4.1 by PowerBall253 :)\n\n");
+    printf("idRehashLinux v1.4.2 by PowerBall253 :)\n\n");
 
     // Read and decompress data from meta.resources
     FILE *meta = fopen("meta.resources", "rb");
